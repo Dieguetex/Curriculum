91013ca01e7f5b26ad47217e627962ee96a0d596
Dieguetex <Diegopmansilla@gmail.com>
2018-10-21 23:40:57 +0200

    Menu Hamburguer perfeccionado

--- javascript/hamburguer.js
+++ javascript/hamburguer.js
@@ -1,14 +1,33 @@
 export class Hamburger{
     constructor() {
-        this.hamburger = document.querySelector("#hamburger")
-        this.hamburger.addEventListener('click', this.abrirMenu.bind(this))
-    }
-    abrirMenu() {
-        this.hamburger.classList.toggle("is-active");
-         
+        /* this.hamburger = document.querySelector("#hamburger")
+        this.hamburger.addEventListener('click', this.abrirMenu.bind(this))*/
 
-        //TODO:
-            //el menú se aparece desde la parte izquierda.
-            //cuando se hace scroll el botón se re-ubica a la parte inferior izquierda de la pantalla y el menú aparece desde la parte inferior
+        this.hamburgerDiv = document.querySelector('#icon')
+        this.divs= document.querySelector('#divTop')
+        this.divHead = document.querySelector('.head')
+
+        this.hamburgerDiv.addEventListener('click', this.abrirMenu.bind(this))
+        document.addEventListener('scroll', this.animationHamburguer.bind(this))
+    }
+
+    //TODO:
+        //el menú se aparece desde la parte izquierda.
+        //cuando se hace scroll el botón se re-ubica a la parte inferior izquierda de la pantalla y el menú aparece desde la parte inferior
+
+
+    abrirMenu() {
+        /* this.hamburger.classList.toggle("is-active"); */
+        this.divs.classList.toggle('open');
+    }
+
+    animationHamburguer(oE){
+        let pos = oE.target.scrollingElement.scrollTop
+        
+        if(pos > 0) {
+            this.divHead.classList.add('animate');
+        } else {
+            this.divHead.classList.remove('animate');
+        }
     }
 }
