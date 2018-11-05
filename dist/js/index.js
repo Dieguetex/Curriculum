f32296c8015410ba013ca880b0483ac0457319ee
Dieguetex <Diegopmansilla@gmail.com>
2018-11-05 14:40:09 +0100

    Animation en Main (section) Beta
    //When scrolling

--- dist/js/index.js
+++ dist/js/index.js
@@ -39,11 +39,14 @@ class Hamburger{
 class Main{
     constructor() {
         this.dropDown = document.querySelectorAll('h2');
-        /* this.addClassHide = document.querySelectorAll('section>div'); */
+        this.nodoSection1 = document.querySelector('#moveIt1');
+        this.nodoSection2 = document.querySelector('#moveIt2');
+        this.nodoSection3 = document.querySelector('#moveIt3');
 
         this.dropDown.forEach( item => {
             item.addEventListener('click', this.desplegar.bind(this));
         });
+        document.addEventListener('scroll', this.isScrolledIntoView.bind(this));
     }
 
     desplegar(oE) {
@@ -60,6 +63,36 @@ class Main{
             //console.log('Cierrate');
             nodoTexto.classList.add('hide');
         }        
+    }
+    isScrolledIntoView(element) {
+        console.log('entra!');
+        let rect = this.nodoSection1.getBoundingClientRect();
+        let elemTop = rect.top;
+        let elemBottom = rect.bottom;
+    
+        // Only completely visible elements return true:
+        let isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+        // Partially visible elements return true:
+        //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+
+/*         if (isVisible) {
+            console.log('se ve el elemento entero!');
+            element.classList.remove('floatItSelfBack');
+            element.classList.add('floatItSelf');
+        } else{
+            element.classList.remove('floatItSelf');
+            element.classList.add('floatItSelfBack');
+        } */
+        
+        if(isVisible){
+            console.log('se ve el elemento entero!');
+            this.nodoSection1.classList.remove('floatItSelfBack');
+            this.nodoSection1.classList.add('floatItSelf');
+        } else{
+            this.nodoSection1.classList.remove('floatItSelf');
+            this.nodoSection1.classList.add('floatItSelfBack');
+        }
+        
     }
 }
 
